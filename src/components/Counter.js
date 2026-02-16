04859aa85e4c64126156cc6d94f1b6ec5c20b0fc
sandeshkhatribct35 <sandeshkhatri456fd@gmail.com>
2026-02-16 10:09:29 +0545

    Code added in Counter.js

---
+++ src/components/Counter.js
@@ -0,0 +1,17 @@
+import React , {userState} from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <h2> Counter: {count}</h2>
+            <button onClick = {() => setCount(count+1)}>Increment</button>
+            <button onClick = {() => setCount(count-1)}>Decrement</button>
+            <button onClick = {() => setCount(0)}>Reset</button>
+
+        </div>
+
+    );
+}
+
+export default Counter;
