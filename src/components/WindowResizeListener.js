4249b81a9bf53cd61861e17ab5cf3571a1d1d6e2
sandeshkhatribct35 <sandeshkhatri456fd@gmail.com>
2026-02-17 03:12:42 +0545

    Code in WindowResizeListener.js

---
+++ src/components/WindowResizeListener.js
@@ -0,0 +1,14 @@
+import React, { useState, userEffect } from 'react';
+
+function WindowResizeListener() {
+    const [width, setWidth] =
+        useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    return <h2>Window width: {width}px</h2>;
+}
+export default WindowResizeListener;
