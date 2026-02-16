563c01f80dae8c77553bec83236f755fa422d1a6
sandeshkhatribct35 <sandeshkhatri456fd@gmail.com>
2026-02-17 02:56:45 +0545

    code added in LoginStatus.js

---
+++ src/components/LoginStatus.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function LoginStatus() {
+ const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+ const toggleLogin = () => {   setIsLoggedIn(!isLoggedIn); };
+
+    return (
+        <div>
+            <h2>{isLoggedIn ? "Welcome back!" : "Please log in."}</h2>
+            {isLoggedIn && <p>You have 3 new messages.</p>}
+
+            {isLoggedIn && <p>Log in to see your messages</p>}
+            <button onClick = {toggleLogin}>{isLoggedIn ? "Log out" : "Log in"}</button>
+        </div>
+    );
+
+
+}
