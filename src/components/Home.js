a26c15f7db7139f22aa75c85b860b9a697b5ad08
sandeshkhatribct35 <sandeshkhatri456fd@gmail.com>
2026-02-17 03:39:12 +0545

    Navigation.js coded

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,23 +4,10 @@ export default function Home() {
   return <h2>Home Page</h2>;
 }
 
-// src/components/About.js
-import React from "react";
-export default function About() {
-  return <h2>About Page</h2>;
-}
+
 
 // src/components/Navigation.js
-import React from "react";
-import { Link } from "react-router-dom";
 
-export default function Navigation() {
-  return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/user/1">User 1</Link>
-    </nav>
-  );
-}
 
 // src/components/UserProfile.js
 import React from "react";
---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/user/1">User 1</Link>
+    </nav>
+  );
+}
